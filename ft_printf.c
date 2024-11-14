24ee44b2e77cc5ae20c5f6b36d3b03542f66cdb2
M.D. <marcio.daniel@msn.com>
2024-11-14 18:44:40 +0000

    feat: add hex type

--- ft_printf.c
+++ ft_printf.c
@@ -5,15 +5,13 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/11 17:31:29 by mhermini          #+#    #+#             */
-/*   Updated: 2024/11/11 20:31:37 by mhermini         ###   ########.fr       */
+/*   Created: 2024/11/12 01:18:31 by mhermini          #+#    #+#             */
+/*   Updated: 2024/11/12 08:13:45 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <stdarg.h>
 
 int	ft_putchar(va_list args)
 {
@@ -35,10 +33,35 @@ int	ft_putstr(va_list args)
 		str = "(null)";
 	while (*str)
 	{
-		write(1, str, 1);
+		count += write(1, str, 1);
 		str++;
-		count++;
 	}
+	return (count);
+}
+
+int	ft_print_hex(unsigned long addr)
+{
+	char	*hex_digits;
+	int		count;
+
+	hex_digits = "0123456789abcdef";
+	count = 0;
+	if (addr >= 16)
+		count += ft_print_hex(addr / 16);
+	count += write(1, &hex_digits[addr % 16], 1);
+	return (count);
+}
+
+int	ft_putptr(va_list args)
+{
+	unsigned long	addr;
+	int				count;
+
+	addr = (unsigned long)va_arg(args, void *);
+	if (addr == 0)
+		return (write(1, "(nil)", 5));
+	count = write(1, "0x", 2);
+	count += ft_print_hex(addr);
 	return (count);
 }
 
@@ -47,14 +70,19 @@ int	handle_format(const char **input, va_list args)
 	int	count;
 
 	count = 0;
-	if (**input == '%' && *(*input + 1) == 'c')
+	if (*(*input + 1) == 'c')
 	{
 		count += ft_putchar(args);
 		*input += 2;
 	}
-	else if (**input == '%' && *(*input + 1) == 's')
+	else if (*(*input + 1) == 's')
 	{
 		count += ft_putstr(args);
+		*input += 2;
+	}
+	else if (*(*input + 1) == 'p')
+	{
+		count += ft_putptr(args);
 		*input += 2;
 	}
 	return (count);
@@ -75,8 +103,7 @@ int	ft_printf(const char *input, ...)
 		}
 		else
 		{
-			write(1, input, 1);
-			count++;
+			count += write(1, input, 1);
 			input++;
 		}
 	}
