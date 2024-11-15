37249f2a76cc0baaa30c952059e40ad48460d1d2
M.D. <marcio.daniel@msn.com>
2024-11-15 10:50:31 +0000

    refactor: improve ft_printf

--- ft_printf.c
+++ ft_printf.c
@@ -6,53 +6,51 @@
 /*   By: mhermini <mhermini@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 01:18:31 by mhermini          #+#    #+#             */
-/*   Updated: 2024/11/14 20:25:33 by mhermini         ###   ########.fr       */
+/*   Updated: 2024/11/15 10:37:16 by mhermini         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int	handle_specifier(char specifier, va_list args)
+static int	dispatch_format_handler(char specifier, va_list args)
 {
 	const char	*str_arg;
 
 	if (specifier == 'c')
 		return (ft_putchar(va_arg(args, int)));
-	else if (specifier == 's')
+	if (specifier == 's')
 	{
 		str_arg = va_arg(args, char *);
 		return (ft_putstr(str_arg));
 	}
-	else if (specifier == 'p')
+	if (specifier == 'p')
 		return (ft_putptr(va_arg(args, void *), 0));
-	else if (specifier == 'd' || specifier == 'i')
+	if (specifier == 'd' || specifier == 'i')
 		return (ft_putnbr(va_arg(args, int), 0));
-	else if (specifier == 'x')
-		return (ft_puthex(va_arg(args, unsigned int), 0));
-	else if (specifier == 'X')
-		return (ft_puthex(va_arg(args, unsigned int), 1));
-	else if (specifier == 'u')
+	if (specifier == 'x' || specifier == 'X')
+		return (ft_puthex(va_arg(args, unsigned int), (specifier == 'X')));
+	if (specifier == 'u')
 		return (ft_putnbr(va_arg(args, unsigned int), 1));
 	return (-1);
 }
 
-static int	parse_format_specifier(const char **input, va_list args)
+static int	process_format_token(const char **input, va_list args)
 {
 	int	char_written;
 
+	char_written = 0;
 	if (*(*input + 1) == '%')
 	{
-		char_written = write(1, "%", 1);
+		char_written += ft_putchar('%');
 		*input += 2;
 		return (char_written);
 	}
-	char_written = handle_specifier(*(*input + 1), args);
+	char_written = dispatch_format_handler(*(*input + 1), args);
 	*input += 2;
 	if (char_written == -1)
 	{
-		write(1, "%", 1);
-		write(1, (*input - 1), 1);
-		char_written = 2;
+		char_written += ft_putchar('%');
+		char_written += ft_putchar(*(*input - 1));
 	}
 	return (char_written);
 }
@@ -67,9 +65,9 @@ int	ft_printf(const char *input, ...)
 	while (*input)
 	{
 		if (*input == '%')
-			char_written += parse_format_specifier(&input, args);
+			char_written += process_format_token(&input, args);
 		else
-			char_written += write(1, input++, 1);
+			char_written += ft_putchar(*input++);
 	}
 	va_end(args);
 	return (char_written);
